9b336e09d6fe6b5b5622de2e2bf4d53e9c7d58f8
bunnyakash <akash199832@gmail.com>
2022-11-07 10:01:55 +0530

    edited wrapper class and added varags

---
+++ src/main/java/b3iam/restap/varargs.java
@@ -0,0 +1,34 @@
+package b3iam.restap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class varargs {
+	public int add(String... x) {
+        int sum=0;
+        for (String n:x) {
+            sum+=Integer.parseInt(n);
+             
+    }
+        return sum;
+    
+
+
+
+}
+
+	
+	@Produces(MediaType.TEXT_HTML)
+	@GET
+	@Path("z/{values}")
+	public String getz(@PathParam("values")String values) {
+	    String[] vals=values.split(",");
+	varargs va=new varargs();
+	return va.add(vals)+"";
+	}
+	}
+
+
--- src/main/java/b3iam/restap/wrapperclass.java
+++ src/main/java/b3iam/restap/wrapperclass.java
@@ -1,5 +1,26 @@
 package b3iam.restap;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("xh")
 public class wrapperclass {
+    @Produces(MediaType.TEXT_HTML)
+    @GET
+    @Path("{cu}")
+    public String wrap(@PathParam("cu")int x) {
+        Integer xob=new Integer(x);
+        String res=xob.toString()+"<br>"+xob.floatValue()+"<br>"+Integer.toBinaryString(x)    ;
+        return "<font size=10px>"+res+"</font>";
+        
+        }
+
+
 
 }
+
+
+
